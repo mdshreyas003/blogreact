7133c531df3d4503b03f7524fab209b90f3da3d4
mdshreyas <mdshreyas003@outlook.com>
2023-08-31 10:09:58 +0530

    search option

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 function PostList() {
   const [posts, setPosts] = useState([]);
   const [selectedPost, setSelectedPost] = useState(null);
-
+const [search,setSearch] = useState("");
   useEffect(() => {
     // Fetch posts when the component mounts
     fetchPosts();
@@ -22,10 +22,33 @@ function PostList() {
   const handlePostClick = (post) => {
     setSelectedPost(post);
   };
+  const searhPosts = async (e)=>{
+    e.preventDefault();
+    var toSearch="";
+    var list = search.split(":");
+    var name = list[1];
+    if(list[0] === "@Company"){
+        toSearch = "Company"
+    }
+    else{
+        toSearch = "Role"
+    }
+
+    try {
+        const response = await axios.get(`https://special-pear-production.up.railway.app/posts/search/by${toSearch}/${name}`); // Replace with your API endpoint
+        setPosts(response.data);
+      } catch (error) {
+        console.error('Error fetching posts:', error);
+      }
+  }
 
   return (
     <div>
       <h1>Posts</h1>
+      <form onSubmit={searhPosts}>
+      <input onChange={e=>setSearch(e.target.value)}/>
+        <input type='submit' value="search"/>
+      </form>
       <ul>
         {posts.map(post => (
           <li key={post.pid} style={{border:'1px solid black'}}>
